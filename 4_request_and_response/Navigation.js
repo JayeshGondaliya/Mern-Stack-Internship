6dd7c51a3713a13d8885db62124be197ca10a58a
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2025-12-14 18:41:00 +0530

    changes

--- 4_request_and_response/Navigation.js
+++ 4_request_and_response/Navigation.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
 
   if (req.url.toLocaleLowerCase() === "/") {
-    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Content-Type", "text/html")
     res.write(`<html>
       <body>
       <nav>
